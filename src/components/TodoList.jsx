54be83e7244147bb3738140d2108f34a28405aea
nozoinho <nantesterdo@gmail.com>
2023-07-31 23:26:22 -0500

    add .prettierrc to reorder tailwind classes

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,33 +2,37 @@ import { Droppable, Draggable } from "@hello-pangea/dnd";
 import TodoItem from "./TodoItem";
 
 const TodoList = ({ todos, removeTodo, updateTodo }) => {
-  return (
-    <Droppable droppableId="todos">
-      {(droppableProvided) => (
-        <div
-          ref={droppableProvided.innerRef}
-          {...droppableProvided.droppableProps}
-          className="mt-8 rounded-t-md overflow-hidden bg-white [&>article]:p-4 dark:bg-gray-800 transition-all duration-1000"
-        >
-          {todos.map((todo, index) => (
-            <Draggable key={todo.id} index={index} draggableId={`${todo.id}`}>
-              {(draggableProvided) => (
-                <TodoItem
-                  todo={todo}
-                  removeTodo={removeTodo}
-                  updateTodo={updateTodo}
-                  ref={draggableProvided.innerRef}
-                  {...draggableProvided.dragHandleProps}
-                  {...draggableProvided.draggableProps}
-                />
-              )}
-            </Draggable>
-          ))}
-          {droppableProvided.placeholder}
-          {/* /*hace referencia al espacio reservado, va despues del map*/}
-        </div>
-      )}
-    </Droppable>
-  );
+    return (
+        <Droppable droppableId="todos">
+            {(droppableProvided) => (
+                <div
+                    ref={droppableProvided.innerRef}
+                    {...droppableProvided.droppableProps}
+                    className=" mt-8 overflow-hidden rounded-t-md bg-white transition-all duration-1000 dark:bg-gray-800 [&>article]:p-4"
+                >
+                    {todos.map((todo, index) => (
+                        <Draggable
+                            key={todo.id}
+                            index={index}
+                            draggableId={`${todo.id}`}
+                        >
+                            {(draggableProvided) => (
+                                <TodoItem
+                                    todo={todo}
+                                    removeTodo={removeTodo}
+                                    updateTodo={updateTodo}
+                                    ref={draggableProvided.innerRef}
+                                    {...draggableProvided.dragHandleProps}
+                                    {...draggableProvided.draggableProps}
+                                />
+                            )}
+                        </Draggable>
+                    ))}
+                    {droppableProvided.placeholder}
+                    {/* /*hace referencia al espacio reservado, va despues del map*/}
+                </div>
+            )}
+        </Droppable>
+    );
 };
 export default TodoList;
